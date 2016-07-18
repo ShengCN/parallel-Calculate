ec9977c1be9986013e5971eeb88fdf8f015d6aa3
CodingBlack <451718145@qq.com>
2016-07-18 18:25:13 +0800

    完成 mpi  并行

--- main.cpp
+++ main.cpp
@@ -7,17 +7,19 @@
 #include <omp.h>
 #include "../../Parallel/city.h"
 #include "../../Parallel/Tour.h"
-using namespace std;
+using std::cout;
+using std::fstream;
+using std::endl;
+using std::vector;
 #define INITIAL_TEMPERATURE 1000
 #define test1 20
 
 int cityDis[test1][test1];
 vector<City> allCitys;
-using namespace std;
 
 //vector<int> bestCase{1,4,16,18,9,7,13,15,11,17,3,5,12,4,2,8,9,19,16,10,20};
 
-// 计算接受的概率
+// ������ܵĸ���
 double acceptanceProbability(int energy, int newEnergy, double temperature)
 {
 	if (newEnergy < energy)
@@ -28,7 +30,7 @@ double acceptanceProbability(int energy, int newEnergy, double temperature)
 
 void initArray()
 {
-	ifstream in("tsp测试数据1.txt", ios::in);
+	ifstream in("tsp��������1.txt", ios::in);
 	int tmp = 0;
 	int count = 0;
 	if (in.is_open())
@@ -46,80 +48,105 @@ void initArray()
 	in.close();
 }
 
-// 获取近似解
+// ��ȡ���ƽ�
 void getApproximateyResult()
 {
 	random_device rd;
-	// 冷却概率
+	// ��ȴ����
 	double coolingRate = 0.003;
-	int n = 10;
+	int n = 64;
 
-	// 初始的解决方案
-	Tour* currentSolution = new Tour(allCitys);
-	currentSolution->generateIndividual();
+	// ��ʼ�Ľ������
+	Tour currentSolution(allCitys);
+	currentSolution.generateIndividual();
 
-	cout << "Initial solution distance: " << currentSolution->getDistance() << endl;
+	std::cout << "Initial solution distance: " << currentSolution.getDistance() << endl;
 
-	// 设置当前为最优的方案
-	Tour best(currentSolution->getTour(), allCitys);
-	//	for (int num_threads = 1; num_threads <= 64;num_threads *= 4)
-	//	{
-	//		vector<Tour> results(num_threads, best);
-	//		omp_set_num_threads(num_threads);
-			// 循环直到系统冷却
-	for (double initialTemprature = INITIAL_TEMPERATURE; initialTemprature > 1; initialTemprature *= 1 - coolingRate)
+	// ���õ�ǰΪ���ŵķ���
+	Tour best(currentSolution.getTour(), allCitys);
+
+	// ���б���
+	for (int thread_count = 1; thread_count <= 64; thread_count *= 4)
 	{
-		for (int i = 0; i < n; ++i)
+		vector<Tour> results(thread_count, best);
+		auto local_currentSolution = best;
+		auto local_best = best;
+		int i;
+
+		// ѭ��ֱ��ϵͳ��ȴ
+		for (double initialTemprature = INITIAL_TEMPERATURE; initialTemprature > 1; initialTemprature *= 1 - coolingRate)
 		{
-			// 生成一个邻居
-			Tour* newSolution = new Tour(currentSolution->getTour(), allCitys);
-
-			// 获得随机位置
-			int tourPos1 = (int)(rd() % newSolution->getTourSize());
-			int tourPos2 = (int)(rd() % newSolution->getTourSize());
-
-			City citySwap1 = newSolution->getCity(tourPos1);
-			City citySwap2 = newSolution->getCity(tourPos2);
-
-			// 交换
-			newSolution->setCity(tourPos2, citySwap1);
-			newSolution->setCity(tourPos1, citySwap2);
-
-			// 获得新的解决方案的话费
-			int currentEnergy = currentSolution->getDistance();
-			int neighbourEnergy = newSolution->getDistance();
-			auto rate = (rd() % 10000)*0.0001;
-			auto accept = acceptanceProbability(currentEnergy, neighbourEnergy, initialTemprature);
-
-			// 决定是否用新的方案
-			if (accept > rate)
+#pragma omp parallel num_threads(thread_count) \
+	default(none) shared(results,thread_count,n,best) private(local_currentSolution,local_best,i)	
+		{
+			local_currentSolution = best;
+			local_best = best;
+#pragma omp for schedule(static,1)
+			for (i = 0; i <n ; ++i)
 			{
-				currentSolution->setTour(newSolution->getTour());
+				// ����һ���ھ�
+				Tour newSolution(local_currentSolution.getTour(), allCitys);
+
+				// ������λ��
+				int tourPos1 = (int)(rd() % newSolution.getTourSize());
+				int tourPos2 = (int)(rd() % newSolution.getTourSize());
+
+				City citySwap1 = newSolution.getCity(tourPos1);
+				City citySwap2 = newSolution.getCity(tourPos2);
+
+				// ����
+				newSolution.setCity(tourPos2, citySwap1);
+				newSolution.setCity(tourPos1, citySwap2);
+
+				// ����µĽ�������Ļ���
+				int currentEnergy = local_currentSolution.getDistance();
+				int neighbourEnergy = newSolution.getDistance();
+				auto rate = (rd() % 10000)*0.0001;
+				auto accept = acceptanceProbability(currentEnergy, neighbourEnergy, initialTemprature);
+
+				// �����Ƿ����µķ���
+				if (accept > rate)
+				{
+					local_currentSolution.setTour(newSolution.getTour());
+				}
+
+				auto getCurrentDis = local_currentSolution.getDistance();
+				auto getBestDis = local_best.getDistance();
+
+				// ��¼�ҵ������ŷ���
+				if (local_currentSolution.getDistance() < local_best.getDistance())
+				{
+					local_best.setTour(local_currentSolution.getTour());
+				}
 			}
 
-			auto getCurrentDis = currentSolution->getDistance();
-			auto getBestDis = best.getDistance();
-
-			// 记录找到的最优方案
-			if (currentSolution->getDistance() < best.getDistance())
-			{
-				best.setTour(currentSolution->getTour());
-			}
-			delete newSolution;
+			auto thread_index = omp_get_thread_num();
+			results[thread_index] = local_best;
 		}
 
-
-		// 打印结果
-//		cout << "当" << num_threads << "线程的时候，最终结果是：" << endl;
+		// local_best ���¸���ֵ
+		for (auto in : results)
+		{
+			if (best.getDistance() > in.getDistance())
+			{
+				best = in;
+			}
+		}
+		local_currentSolution = best;
+		local_best = best;
+		}
+		
+		cout <<"=============================================="<< endl;
+		cout << "When thread is: " << thread_count << endl;
+		cout << "Final solution distance is : " << best.getDistance() << endl;
+		cout << "����·����:" << endl;
+		auto route = best.getTour();
+		for (auto index : route)
+		{
+			cout << index.getId() << "----->";
+		}
+		cout << endl;
 	}
-	cout << "Final solution distance is : " << best.getDistance() << endl;
-	cout << "最优路径是:" << endl;
-	auto route = best.getTour();
-	for (auto index : route)
-	{
-		cout << index.getId() << "----->";
-	}
-	cout << endl;
 }
 
 void initCity()
