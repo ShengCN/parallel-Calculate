82bd6972a3c6f94f32d4d1c393d785dec4de7ff8
CodingBlack <451718145@qq.com>
2016-07-20 19:00:30 +0800

    完成 K 路归并算法。

--- K路归并/main.cpp
+++ K路归并/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 #include <fstream>
 #include <string>
 #include <omp.h>
@@ -13,43 +14,66 @@ using std::endl;
 
 /**************** shared parameter *******************/
 int k; // k route
-int leftSize; // left numbers 
+int leftSize; // left numbers
+vector<int> result;//merged array
 vector<vector<int>> k_route_matrix;
 vector<int> leftNumbers;
 int thread;
 
+struct TwoDimIndex
+{
+	int x;
+	int y;
+	TwoDimIndex(int x,int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+
 /*
- * description: init array function
- * input:		k_route_matrix----- the array to be sorted and merge
- *				n----- the input array number (1¡¢2¡¢3)
- * output:		if the test array file is opened right
- *
- */
+* description: init array function
+* input:		k_route_matrix----- the array to be sorted and merge
+*				n----- the input array number (1?¡é2?¡é3)
+* output:		if the test array file is opened right
+*
+*/
 bool initArray(int n);
 
 /*
- * description: k-merge function
- * input:		k_route_matrix----- the array to be sorted and merge
- * output:      sorted array
- *
- */
+* description: k-merge function
+* input:		k_route_matrix----- the array to be sorted and merge
+* output:      sorted array
+*
+*/
 vector<int> k_merge();
 
 /*
- * description: input the array from file
- * input:		k_route_matrix----- the array to be sorted and merge
- *				          file----- the input file number 
- * output:      is file opened 
+* description: input the array from file
+* input:		k_route_matrix----- the array to be sorted and merge
+*				          file----- the input file number
+* output:      is file opened
+*
+*/
+bool inputArray(const string file);
+
+/*
+ * description: binary search function(row or column)
+ * input:		start----start Point
+ *				  end----end Point
+ *          searchKey----key to be searched
+ *                isX----row or column
+ * output:      the first smaller one than searchKey
  *
  */
-bool inputArray(const string file);
+int binarySearch(TwoDimIndex start, TwoDimIndex end, int searchKey, bool isX);
 
 // debug function
 void printAll()
 {
-	for (auto i:k_route_matrix)
+	for (auto i : k_route_matrix)
 	{
-		for (auto j:i)
+		for (auto j : i)
 		{
 			cout << j << "\t";
 		}
@@ -58,7 +82,7 @@ void printAll()
 	cout << endl << endl;
 }
 
-int main(int argvc,char* argv[])
+int main(int argvc, char* argv[])
 {
 	thread = strtol(argv[1], NULL, 10);
 	vector<int> k_route_matrix;
@@ -99,6 +123,29 @@ vector<int> k_merge()
 	double start, finish;
 	start = omp_get_wtime();
 
+	// generate k-route array
+	int k_count,row_count,leftSize = result.size() % k,rows = result.size()/k;
+	leftNumbers.resize(leftSize);
+
+#pragma omp parallel \
+	default(none) shared(k_route_matrix,leftNumbers,result) private()
+	{
+#pragma omp for
+		for (row_count = 0; row_count < rows; ++row_count)
+		{
+			vector<int> tmp;
+			for (k_count = 0; k_count < k; ++k_count)
+			{
+				tmp.push_back(result[(row_count + 1)*(k_count + 1) - 1]);
+			}
+			k_route_matrix.push_back(tmp);
+		}
+#pragma omp for
+		for (auto i = 0; i < leftSize; ++i)
+		{
+			leftNumbers[i] = result[rows*k + i];
+		}
+	}
 	/********------|||||///////************/
 	auto row = k_route_matrix.size();
 	auto column = k_route_matrix[0].size();
@@ -131,109 +178,118 @@ vector<int> k_merge()
 			}
 		}
 	}
-		//	printAll();
+	//	printAll();
 
-			//  /////
-			// fisrt session
-		int slopeTimes = row - 1;
-		int count;
+	//  /////
+	// fisrt session
+	int slopeTimes = row - 1;
+	int count;
 
-		for (int i = slopeTimes; i > 0; --i)
-		{
+	for (int i = slopeTimes; i > 0; --i)
+	{
 #pragma omp parallel \
 	default(none) shared(k_route_matrix) private(count)
+	{
+		count = 1;
+#pragma omp for schedule(static,(column + row - 1)/thread)
+		for (int j = 0; j < column + row - 1; ++j)
 		{
-			count = 1;
-#pragma omp for
-			for (int j = 0; j < column + row - 1; ++j)
+			vector<int> buffer;
+			int local_y = j, local_x = 0;
+			for (; local_x < row; local_x = local_x + i)
 			{
-				vector<int> buffer;
-				int local_y = j, local_x = 0;
-				for (; local_x < row; local_x = local_x + i)
+				if (local_y >= 0 && local_y < column)
 				{
-					if (local_y >= 0 && local_y < column)
-					{
-						buffer.push_back(k_route_matrix[local_x][local_y]);
-					}
-					local_y -= count;
+					buffer.push_back(k_route_matrix[local_x][local_y]);
 				}
-				std::sort(buffer.begin(), buffer.end());
-				// write back
-				int buffer_index = 0;
-				local_y = j, local_x = 0;
-				for (; local_x < row; local_x = local_x + i)
-				{
-					if (local_y >= 0 && local_y < column)
-					{
-						k_route_matrix[local_x][local_y] = buffer[buffer_index++];
-					}
-					local_y -= count;
-				}
+				local_y -= count;
 			}
-			count++;
-			//		printAll();
+			std::sort(buffer.begin(), buffer.end());
+			// write back
+			int buffer_index = 0;
+			local_y = j, local_x = 0;
+			for (; local_x < row; local_x = local_x + i)
+			{
+				if (local_y >= 0 && local_y < column)
+				{
+					k_route_matrix[local_x][local_y] = buffer[buffer_index++];
+				}
+				local_y -= count;
+			}
 		}
-		}
+		count++;
+		//		printAll();
+	}
+	}
 
-		// second session
-		slopeTimes = column - 1;
-		for (int i = 1; i <= slopeTimes; ++i)
-		{
+	// second session
+	slopeTimes = column - 1;
+	for (int i = 1; i <= slopeTimes; ++i)
+	{
 #pragma omp parallel \
 	default(none) shared(k_route_matrix) private(count)
+	{
+		count = 1;
+#pragma omp for schedule(static,(column + row - 1)/thread)
+		for (int j = 0; j < row + column - 1; ++j)
 		{
-			count = 1;
-#pragma omp for
-			for (int j = 0; j < row + column - 1; ++j)
+			vector<int> buffer;
+			int local_x = j, local_y = 0;
+			for (; local_y < column; local_y = local_y + i)
 			{
-				vector<int> buffer;
-				int local_x = j, local_y = 0;
-				for (; local_y < column; local_y = local_y + i)
+				if (local_x >= 0 && local_x < row)
 				{
-					if (local_x >= 0 && local_x < row)
-					{
-						buffer.push_back(k_route_matrix[local_x][local_y]);
-					}
-					local_x -= count;
+					buffer.push_back(k_route_matrix[local_x][local_y]);
 				}
-				std::sort(buffer.begin(), buffer.end());
-				// write back
-				int buffer_index = buffer.size() - 1;
-				local_x = j, local_y = 0;
-				for (; local_y < column; local_y = local_y + i)
-				{
-					if (local_x >= 0 && local_x < row)
-					{
-						k_route_matrix[local_x][local_y] = buffer[buffer_index--];
-					}
-					local_x -= count;
-				}
+				local_x -= count;
 			}
-			count++;
+			std::sort(buffer.begin(), buffer.end());
+			// write back
+			int buffer_index = buffer.size() - 1;
+			local_x = j, local_y = 0;
+			for (; local_y < column; local_y = local_y + i)
+			{
+				if (local_x >= 0 && local_x < row)
+				{
+					k_route_matrix[local_x][local_y] = buffer[buffer_index--];
+				}
+				local_x -= count;
+			}
 		}
-		}
-//	printAll();
+		count++;
+	}
+	}
+	//	printAll();
 
 	// insert point
 	std::sort(leftNumbers.begin(), leftNumbers.end());
 
 	// return result vector
-	vector<int> result;
+	
 	auto iter = leftNumbers.begin();
+#pragma omp parallel \
+	default(none) shared(k_route_matrix,iter,result) private()
+#pragma omp for
 	for (int i = 0; i < row; ++i)
 	{
 		for (int j = 0; j < column; ++j)
 		{
-			if ((iter != leftNumbers.end()) && k_route_matrix[i][j] > (*iter))
+			if(iter!=leftNumbers.end() && k_route_matrix[i][j]>*iter)
 			{
-				result.push_back(*iter++);
+				result[(i + 1)*(j + 1) - 1] = *iter;
 				j--;
+#pragma omp critical
+				iter++;
+				
 			}
-			result.push_back(k_route_matrix[i][j]);
+			else
+			{
+				result[(i + 1)*(j + 1) - 1] = k_route_matrix[i][j];
+			}
 		}
 	}
 	finish = omp_get_wtime();
-	
+
 	cout << endl << endl;
 	cout << "=================================" << endl;
 	cout << "Thread is: " << thread << endl;
@@ -254,43 +310,52 @@ bool inputArray(const string file)
 	{
 		int count = 0, int_tmp;
 		in >> k;
-		while (in >> int_tmp)
-		{
-			count++;
-		}
-		in.close();
-		leftSize = count % k;
-		int rows = count / k;
-
-		// load in
-		count = 0;
-		int row_count = 0;
-		in.open(file);
-		in >> k;
-		vector<int> buffer(k);
-		while (in >> int_tmp)
-		{
-			if (row_count < rows)
-			{
-				if (count < k)
-				{
-					buffer[count++] = int_tmp;
-				}
-				else
-				{
-					k_route_matrix.push_back(buffer);
-					count = 0;
-					buffer[count++] = int_tmp;
-					row_count++;
-				}
-			}
-			else
-			{
-				leftNumbers.push_back(int_tmp);
-			}
-		}
-		k_route_matrix.push_back(buffer);
+		std::copy(std::istream_iterator<int>(in), std::istream_iterator<int>(), std::back_inserter(result));
 		cout << "\n\n" << file << " loading end." << "\n\n";
 		return true;
+	}
+}
+
+int binarySearch(TwoDimIndex start, TwoDimIndex end, int searchKey, bool isX)
+{
+	if (isX)
+	{
+		// row search
+		auto rowIndex = start.x;
+		auto local_start = start.y, local_end = end.y;
+		while (local_start <= local_end)
+		{
+			auto mid = local_start + (local_end - local_start) / 2;
+			if (k_route_matrix[rowIndex][mid] < searchKey)
+				local_start = mid + 1;
+			else if (k_route_matrix[rowIndex][mid] > searchKey)
+				local_end = mid - 1;
+			else
+				return mid;
+		}
+		if (searchKey <= k_route_matrix[start.x][start.y])
+			return 0;
+		else
+			return end.y;
+	}
+	else
+	{
+		// column search
+		auto columnIndex = start.y;
+		auto local_start = start.x, local_end = end.x;
+		while (local_start <= local_end)
+		{
+			auto mid = local_start + (local_end - local_start) / 2;
+			if (k_route_matrix[mid][columnIndex] < searchKey)
+				local_start = mid + 1;
+			else if (k_route_matrix[mid][columnIndex] > searchKey)
+				local_end = mid - 1;
+			else
+				return mid;
+		}
+		if (searchKey <= k_route_matrix[start.x][start.y])
+			return 0;
+		else
+			return end.x;
 	}
 }
